a35c40ea63906ca1ab7ff13672a728ffb0439440
Blair <blairacarroll@gmail.com>
2020-04-16 02:10:11 -0400

    deleted useless calendar controller, struct, views

--- cts_api/config/routes.rb
+++ cts_api/config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  resource :calendar, only: [:show], controller: :calendar
-  root to: "calendar#show"
-  # get 'calendar/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/schedule', to: 'schedules#index'
   # get '/sched', to: 'schedules#each_day_in_month'
